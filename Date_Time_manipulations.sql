39263fffe0f2626716ca4c16fbff044c60e5c36a
tomaztk <tomaz.kastrun@gmail.com>
2018-09-03 23:14:59 +0200

    Add files via upload

--- Date_Time_manipulations.sql
+++ Date_Time_manipulations.sql
@@ -25,6 +25,14 @@ SELECT
 	,MONTH(GETDATE()) AS Month_RightNow
 	,DAY(GETDATE()) AS Day_RightNow
 
+-- Is Leap Year (we check for presence/existence of 29.Feb)
+SELECT
+	 ISDATE('2016/02/29') AS CheckLeapYear -- if 0, date does not exists and is not leap year
+	,ISDATE('2019/02/29') AS CheckLeapYear -- if 1, date exists and this year is a leap year
+	,CAST(GETDATE() AS INT) % 400  -- if modulo = 0 (equals to zero), year is a leap year
+	,CAST(GETDATE()-800 AS INT) % 400  -- if modulo = 0 (equals to zero), year is a leap year
+	
+select getdate()-800
 
 -- Using Date format
 SELECT
@@ -38,8 +46,46 @@ SELECT
 
 -- Weeks
 SELECT
-	 DATEADD(DAY,DATEDIFF(DAY,1,GETDATE()),0) AS Yesterday
-	,DATEADD(DAY,DATEDIFF(DAY,0,GETDATE()),0) AS Today
-	,DATEADD(DAY,DATEDIFF(DAY,0,GETDATE()),1) AS Tomorrow
+	 DATEADD(WEEK,DATEDIFF(WEEK,7,GETDATE()),0) AS LastWeek_startOf
+	,DATEADD(WEEK,DATEDIFF(WEEK,0,GETDATE()),0) AS ThisWeek_startOf
+	,DATEADD(WEEK,DATEDIFF(WEEK,0,GETDATE()),7) AS NextWeek_startOf
 
-						
+-- Months (works for all months; with 30 or 31 days, or with February)
+SELECT
+	 DATEADD(MONTH,DATEDIFF(MONTH,31,GETDATE()),0) AS LastMonth_startOf
+	,DATEADD(MONTH,DATEDIFF(MONTH,0,GETDATE()),0) AS ThisMonth_startOf
+	,DATEADD(MONTH,DATEDIFF(MONTH,-1,GETDATE()),0) AS NextMonth_startOf
+
+SELECT 
+	 EOMONTH(GETDATE()) AS CurrentMonthEnd,
+    ,EOMONTH(GETDATE(), -1) AS PreviousMonthEnd
+    ,EOMONTH(GETDATE(), 1) AS NextMonthEnd
+
+
+-- Years (works with leap years)
+SELECT
+	 DATEADD(year, DATEDIFF(year, 365, (GETDATE())), 0) AS LastYear_startOf
+	,DATEADD(year, DATEDIFF(year, 0, (GETDATE())), 0) AS ThisYear_startOf
+	,DATEADD(year, DATEDIFF(year, -1, (GETDATE())), 0) AS NextYear_startOf
+
+-----------------------------------------------
+-----------------------------------------------
+-- 2. Getting dates in particular point-in-time
+-----------------------------------------------
+-----------------------------------------------
+
+-- return the date on today in a month
+SELECT
+	DATEADD(MONTH, DATEDIFF(MONTH, -1, (GETDATE())), DAY(GETDATE()-1)) AS OneMonthFromNow
+   ,CAST(DATEADD(MONTH, 1, CAST(GETDATE() AS DATE)) AS SMALLDATETIME) AS OneMonthFromNow2
+
+
+SELECT
+	DATEADD(DAY, DATEDIFF(DAY, -1, GETDATE()), 365) AS OneYearFromNow
+		       , dateadd(day, datediff(day, -1, (getdate())), 365) as toèno_èez_eno_leto_od_danes
+
+-----------------------------------------------
+-----------------------------------------------
+-- 3. Starting with simple time
+-----------------------------------------------
+-----------------------------------------------
