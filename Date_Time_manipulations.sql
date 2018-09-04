a6afece4e09362875a325f0b409f96d333bb865f
tomaz <tomaz.kastrun@outlook.com>
2018-09-04 21:34:26 +0200

    uptodate

--- Date_Time_manipulations.sql
+++ Date_Time_manipulations.sql
@@ -147,37 +147,6 @@ SELECT
 FROM cte 
 OPTION (maxRecursion 10000)
 
-
-
-
-
-
-
------------------------------------------------
------------------------------------------------
--- 5. Date functions
------------------------------------------------
------------------------------------------------
-
-
-SET LANGUAGE us_english;
-SELECT DATENAME(WEEKDAY, '20190904') [US_English];
- 
-SET LANGUAGE Slovenian;
-SELECT DATENAME(WEEKDAY, '20180904') [Slovenian];
-
-
--- Starting week with day
-SET LANGUAGE Slovenian;  
-SELECT @@DATEFIRST AS weekStart;  
-
-SET LANGUAGE us_english;  
-SELECT @@DATEFIRST AS WeekStart;  
-
-
---OR
-SET DATEFIRST 1; -- week starts with monday
-
 -----------------------------------------------
 -----------------------------------------------
 -- 4. Date intervals
@@ -193,3 +162,104 @@ SELECT
 	 CAST(DATEADD(DAY, 1, EOMONTH(GETDATE(),-1)) AS DATETIME) AS FromCurrentMonth
 	,CAST(EOMONTH(GETDATE()) AS DATETIME) AS ToCurrentMonth
 
+
+SELECT	
+	 DATEADD(QUARTER, DATEDIFF(QUARTER, 0, (GETDATE())), 0) as FromStartCurrentQuarter
+	,DATEADD(DAY,-1,DATEADD(QUARTER, DATEDIFF(QUARTER, -1, (GETDATE())), 0)) as ToEndCurrentQuarter
+	
+
+
+
+-----------------------------------------------
+-----------------------------------------------
+-- 5. Support Date functions
+-----------------------------------------------
+-----------------------------------------------
+
+
+-- Set language
+SET LANGUAGE us_english;
+SELECT DATENAME(WEEKDAY, '20190904') [US_English];
+ 
+SET LANGUAGE Slovenian;
+SELECT DATENAME(WEEKDAY, '20180904') [Slovenian];
+
+
+-- Starting week with day
+SET LANGUAGE Slovenian;  
+SELECT @@DATEFIRST AS weekStart;  
+
+SET LANGUAGE us_english;  
+SELECT @@DATEFIRST AS WeekStart;  
+
+
+-- datefirst or @@datefirst
+SET DATEFIRST 1; -- week starts with monday
+
+
+
+-----------------------------------------------
+-----------------------------------------------
+-- 6. Starting Simple Time
+-----------------------------------------------
+-----------------------------------------------
+
+
+SELECT
+	 GETDATE() AS RightNow
+	,YEAR(GETDATE()) AS Year_RightNow
+	,MONTH(GETDATE()) AS Month_RightNow
+	,DAY(GETDATE()) AS Day_RightNow
+
+-- Is Leap Year (we check for presence/existence of 29.Feb)
+SELECT
+	 ISDATE('2019/02/29') AS CheckLeapYear -- if 1, date exists and this year is a leap year; if 0 date does not exists and is not leap year
+	,CASE WHEN (YEAR(GETDATE()) % 4 = 0 AND YEAR(GETDATE()) % 100 <> 0) OR YEAR(GETDATE()) % 400 = 0 THEN 'Leap Year' ELSE 'Non Leap Year' END AS CheckLeapYear
+
+
+-- Name of Days, Months
+SELECT 
+	DATENAME(WEEKDAY, GETDATE()) AS [DayName]
+   ,DATENAME(MONTH, GETDATE()) AS [MonthName]
+   ,DATEPART(WEEK, GETDATE()) AS [WeekNumber]
+   ,DATEPART(ISO_WEEK, GETDATE()) AS [ISO_WeekNumber]
+
+
+-- Using Date format or FORMAT or CAST / CONVERT
+SELECT
+	CAST(GETDATE() AS DATE) AS Date_RightNow
+	,FORMAT(GETDATE(), 'dd/MM/yyyy') AS DATE_dd_MM_yyyy
+	,FORMAT(GETDATE(), 'yyyy-MM-dd') AS DATE_yyyy_MM_dd
+	,FORMAT(GETDATE(), 'MM-dd') AS DATE_MM_dd
+	,FORMAT(GETDATE(), 'dd/MM/yyyy', 'en-US' ) AS DATE_US 
+	,FORMAT(GETDATE(), 'dd/MM/yyyy', 'sl-SI' ) AS DATE_SLO
+
+-- Days
+SELECT
+	 DATEADD(DAY,DATEDIFF(DAY,1,GETDATE()),0) AS Yesterday
+	,DATEADD(DAY,DATEDIFF(DAY,0,GETDATE()),0) AS Today
+	,DATEADD(DAY,DATEDIFF(DAY,0,GETDATE()),1) AS Tomorrow
+
+-- Weeks
+SELECT
+	 DATEADD(WEEK,DATEDIFF(WEEK,7,GETDATE()),0) AS LastWeek_startOf
+	,DATEADD(WEEK,DATEDIFF(WEEK,0,GETDATE()),0) AS ThisWeek_startOf
+	,DATEADD(WEEK,DATEDIFF(WEEK,0,GETDATE()),7) AS NextWeek_startOf
+
+-- Months (works for all months; with 30 or 31 days, or with February)
+SELECT
+	 DATEADD(MONTH,DATEDIFF(MONTH,31,GETDATE()),0) AS LastMonth_startOf
+	,DATEADD(MONTH,DATEDIFF(MONTH,0,GETDATE()),0) AS ThisMonth_startOf
+	,DATEADD(MONTH,DATEDIFF(MONTH,-1,GETDATE()),0) AS NextMonth_startOf
+
+SELECT 
+	 EOMONTH(GETDATE()) AS CurrentMonthEnd
+    ,EOMONTH(GETDATE(), -1) AS PreviousMonthEnd
+    ,EOMONTH(GETDATE(), 1) AS NextMonthEnd
+
+
+-- Years (works with leap years)
+SELECT
+	 DATEADD(year, DATEDIFF(year, 365, (GETDATE())), 0) AS LastYear_startOf
+	,DATEADD(year, DATEDIFF(year, 0, (GETDATE())), 0) AS ThisYear_startOf
+	,DATEADD(year, DATEDIFF(year, -1, (GETDATE())), 0) AS NextYear_startOf
